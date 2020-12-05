2bd9ca4ffb73d1a9a3c1895a9c59dde63ae4039d
NikitaLimon <58005208+NikitaLimon@users.noreply.github.com>
2020-12-05 12:26:11 +0300

    Update event.py

--- cogs/event.py
+++ cogs/event.py
@@ -19,7 +19,7 @@ class Eve(commands.Cog):
 		print("Event cog connect")
 	@commands.Cog.listener()
 	async def on_member_join(self, member):
-		idc = self.prefixes.find_one({"guild_id": member.guild.id})["welcome"]
+		idc = self.prefixes.find_one({"_guild_id": member.guild.id})["welcome"]
 		if idc == 0:
 			pass
 		else:
