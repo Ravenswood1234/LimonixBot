b979f4f25405cbd0627cedcd05c1a83417977fff
NikitaLimon <58005208+NikitaLimon@users.noreply.github.com>
2020-12-05 11:19:03 +0300

    Update economic.py

--- cogs/economic.py
+++ cogs/economic.py
@@ -44,7 +44,7 @@ class Econom(commands.Cog):
 
 				)
 		else:
-			self.prefixes.update_one({"_guild_id": ctx.guild.id}, {"$set": {"prefix": channel.id}})
+			self.prefixes.update_one({"_guild_id": ctx.guild.id}, {"$set": {"prefix": str(channel.id)}})
 			await ctx.send(
 				embed=discord.Embed(
 					title="Успешно",
