c41fb53984c4f2200853687f483132cc93c9cc24
NikitaLimon <58005208+NikitaLimon@users.noreply.github.com>
2020-12-07 20:11:47 +0300

    Update event.py

--- cogs/event.py
+++ cogs/event.py
@@ -36,7 +36,7 @@ class Eve(commands.Cog):
 			pass
 		else:
 			channel=self.client.get_channel(int(idc))
-			file = discord.File("./hello.gif", filename="hello.gif")
+			
 			embb = discord.Embed(
 				title=f"{member.name}, добро пожаловать!",
 				colour=discord.Color.gold(), 
@@ -45,8 +45,8 @@ class Eve(commands.Cog):
 Он уже {member.guild.member_count} участник нашего сервера!""")
 			embb.set_footer(text=f"Сервер: {member.guild.name}")
 			embb.set_thumbnail(url=member.avatar_url)
-			embb.set_image(url="attachment://hello.gif")
-			await channel.send(file=file, embed=embb)
+			
+			await channel.send( embed=embb)
 
 	# @commands.Cog.listener()
 	# async def on_typing(self, channel, user, when):
