3d6e55a5bfbbb55c3670cd04961b35746b78f186
NikitaLimon <58005208+NikitaLimon@users.noreply.github.com>
2020-12-07 17:55:29 +0300

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from Cybernator import Paginator as pag
 import discord
 class Help(commands.Cog):
 	"""docstring for User"""
@@ -6,33 +7,24 @@ class Help(commands.Cog):
 		self.client = client
 	@commands.command()
 	async def help(self, ctx):
-		emb = discord.Embed(
-			title="Список комманд",
-			colour=discord.Color.blue()
+		cog = self.client.cogs['Экономиика']
+		name = cog.qualified_name
+		commands = cog.get_commands()
+		emb = embed.Embed(
+			title="Список команд",
+			colour=discord.Color.green()
 			)
-		emb.add_field(
-			name="Утилиты",
-			value="wiki, userinfo, server, bot, roleinfo, spotify, ping, ava, weather, google, say,  report",
-			inline=False
-			)
-		emb.add_field(
-			name="Модерация",
-			value="clear, say, poll, mute, slowmode, unmute, ban, kick, prefix, report-channel, welcome",
-			inline=False
-			)
-		emb.add_field(
-			name="Фан",
-			value="8ball, img, anekdot, fox, cat, dog",
-			inline=False
-			)
-		emb.add_field(
-			name="Экономиика",
-			value="balance, award, take",
-			inline=False
-			)
-		emb.set_footer(text=f"При баге {ctx.prefix}bugs <bug>, при новой идеии {ctx.prefix}idea <idea>")
-		emb.set_author(name=f"Префикс: {ctx.prefix}")
-		await ctx.send(embed=emb)
+		if name is not None:
+			comm_list = []
+			for command in commands:
+				if not command.hidden:
+					comm_list.append(f"`{ctx.prefix}{command.name}`")
+
+			embed_all.add_field(
+				name=f"Экономиика",
+				value=f"{', '.join(comm_list)}",
+				inline=False)
+		await ctx.send(embed=embed_all)
 
 def setup(client):
 	client.add_cog(Help(client))
